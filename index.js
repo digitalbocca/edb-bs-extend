1b39c0319d37bdb687cb0c3cbbfc15f4c7f088bb
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2018-02-21 11:14:11 -0300

    Publicado

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/**
+ * edb-bs-extend
+ *
+ * @file index.js
+ * @namespace root
+ * @description Arquivo de Entrada
+ * @since v0.2.0
+ *
+ * @copyright (c)2018 - Estúdio Digital Bocca - https://estudiodigitalbocca.com.br/
+ * @author Gabriel Bertola Bocca - gabriel at estudiodigitalbocca.com.br
+ *
+ * @version v1.0.0
+ */
+
+console.log('©2018 - Estúdio Digital Bocca')
